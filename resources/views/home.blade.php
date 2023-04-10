1c3815de3c4767b28f5e935236afe543d307f236
amineelaabdi14 <amineelaabdi@gmail.com>
2023-04-10 04:18:40 +0000

    bootstrap ,login page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="{{route('login')}}" method="POST">
-        @csrf
-        <input type="email" name="email">
-        <input type="password" name="password">
-        <button type="submit">dekhelni pls hh</button>
-    </form>
-    <form action="{{route('register')}}" method="POST">
-        @csrf
-        <input type="email" name="email">
-        <input type="text" name="name">
-        <input type="password" name="password">
-        <button type="submit">9eyedni pls hh</button>
-    </form>
+    <head>
+        <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+        <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+    </head>
+<body id="loginContent">
+    <div id="LoginformContainer" class="container">
+        <img src="../resources/logo.png" alt="YouCode" class="mt-4 logoContainer">
+        <form action="includes/script.php" class="form d-flex flex-column " method="POST">
+            
+            <div id="emailFormContainer">
+                <label for="email" class="MyLabels">EMAIL</label><br>
+                <input type="email" name="email"  class="MyInputs" oninput="validateEmail()" autocomplete="off">
+                <div id="emailError" class="text-danger"></div>
+            </div>
+
+            <div class="mb-3">
+                <label for="password" class="MyLabels">PASSWORD</label><br>
+                <input type="password" name="password" class="MyInputs">
+                <div id="comfirEmailError" class="text-danger mb-0 "></div>
+            </div>
+            <div id="fullName"></div>
+            <div class="d-flex justify-content-between align-items-center mt-3" style="">
+                <span><a href="#" onclick="createAccount()">Create an account</a></span>
+                <button type="submit"  name="signMeIn" disabled>Sign In</button>
+            </div>
+            <h1 class="bg-danger">heheeh</h1>
+
+        </form>
+    </div>
+    
 </body>
 </html>
