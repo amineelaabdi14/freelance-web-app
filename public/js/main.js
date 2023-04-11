e0c2a4a1dac22ea709a42c0d55f6ab6144e9f445
amineelaabdi14 <amineelaabdi@gmail.com>
2023-04-11 16:00:56 +0000

    LIINKIN THINGS

--- public/js/main.js
+++ public/js/main.js
@@ -5,11 +5,8 @@ function validateEmail() {
     if(email.match(pattern))
     {    
         emailError.innerText="";
-        if(TempChecker==true)
-        {
-            document.querySelector('button[type=submit]').disabled=false;
-            document.querySelector('button[type=submit]').style.filter="saturate(100%)";
-        }
+        document.querySelector('button[type=submit]').disabled=false;
+        document.querySelector('button[type=submit]').style.filter="saturate(100%)";
     }
    else
    {
@@ -17,27 +14,23 @@ function validateEmail() {
         document.querySelector('button[type=submit]').style.filter="saturate(0%)";
         emailError.innerText="The email address format is incorrect";
    }
-   if(document.querySelector('form[method=POST]').children[1].children[2].getAttribute('name')=="confirmEmail")
-    {
-        checkConfirmEmail();
-    }
     
   }
   
-  let TempChecker=true;
 function createAccount ()
-{   TempChecker=false;
+{   
+    var registerUrl = '/register';
+    document.getElementById('auth-form').action=registerUrl;
     document.querySelector('button[type=submit]').style.filter="saturate(0%)";
     emailError.innerText="The email address format is incorrect";
     comfirEmailError.innerText="";
     emailError.innerText="";
     document.getElementsByTagName('form')[0].reset();
-    document.querySelector('label[for=password]').innerText="CONFIRM EMAIL";
-    document.querySelector('label[for=password]').setAttribute('for','confirmEmail');
+    document.querySelector('label[for=password]').innerText="Name";
+    document.querySelector('label[for=password]').setAttribute('for','name');
 
-    document.querySelector('input[name=password]').setAttribute('type','email');
-    document.querySelector('input[name=password]').setAttribute('name','confirmEmail');
-    document.querySelector('input[name=confirmEmail]').setAttribute('oninput','checkConfirmEmail()');
+    document.querySelector('input[name=password]').setAttribute('type','text');
+    document.querySelector('input[name=password]').setAttribute('name','name');
 
     document.querySelector('a[onclick="createAccount()"]').innerText="I already create an account";
 
@@ -46,23 +39,23 @@ function createAccount ()
 
     document.querySelector('a[onclick="createAccount()"]').setAttribute("onclick","alrMember()")
 
-    fullName.innerHTML=`<label for="fullname" class="MyLabels">Full Name</label><br><input type="text" name="fullName" id="" class="MyInputs">`;
-    document.getElementById('LoginformContainer').style.height="500px";                 
+    fullName.innerHTML=`<label for="password" class="MyLabels">Password</label><br><input type="password" name="password" class="MyInputs">`;
+    document.getElementById('LoginformContainer').style.height="500px";   
+
 }
 function alrMember()
 {   
-    TempChecker=true;
     document.querySelector('button[type=submit]').style.filter="saturate(0%)";
     emailError.innerText="The email address format is incorrect";
     fullName.innerHTML="";
     comfirEmailError.innerText="";
     emailError.innerText="";
     document.getElementsByTagName('form')[0].reset();
-    document.querySelector('label[for=confirmEmail]').innerText="PASSWORD";
-    document.querySelector('label[for=confirmEmail]').setAttribute('for','password');
+    document.querySelector('label[for=name]').innerText="PASSWORD";
+    document.querySelector('label[for=name]').setAttribute('for','password');
     
-    document.querySelector('input[name=confirmEmail]').setAttribute('type','password');
-    document.querySelector('input[name=confirmEmail]').setAttribute('name','password');
+    document.querySelector('input[name=name]').setAttribute('type','password');
+    document.querySelector('input[name=name]').setAttribute('name','password');
     document.querySelector('input[name=password]').removeAttribute('oninput');
 
     document.querySelector('a[onclick="alrMember()"]').innerText="Create an account";
@@ -74,44 +67,3 @@ function alrMember()
 
     document.getElementById('LoginformContainer').style.height="400px";  
 }
-function checkConfirmEmail(){
-    let email=document.querySelector('input[name=email]').value;
-    let confirmEmail=document.querySelector('input[name=confirmEmail]').value;
-    if(email!=confirmEmail)
-    {
-        comfirEmailError.innerText="You must enter the same email address";
-        document.querySelector('button[type=submit]').disabled=true;
-        document.querySelector('button[type=submit]').style.filter="saturate(0%)";
-    }
-    else{
-        comfirEmailError.innerText="";
-        document.querySelector('button[type=submit]').disabled=false;
-        document.querySelector('button[type=submit]').style.filter="saturate(100%)";
-    }
-    }
-
-    function alrMember()
-    {   
-        TempChecker=true;
-        document.querySelector('button[type=submit]').style.filter="saturate(0%)";
-        emailError.innerText="The email address format is incorrect";
-        fullName.innerHTML="";
-        comfirEmailError.innerText="";
-        emailError.innerText="";
-        document.getElementsByTagName('form')[0].reset();
-        document.querySelector('label[for=confirmEmail]').innerText="PASSWORD";
-        document.querySelector('label[for=confirmEmail]').setAttribute('for','password');
-        
-        document.querySelector('input[name=confirmEmail]').setAttribute('type','password');
-        document.querySelector('input[name=confirmEmail]').setAttribute('name','password');
-        document.querySelector('input[name=password]').removeAttribute('oninput');
-    
-        document.querySelector('a[onclick="alrMember()"]').innerText="Create an account";
-    
-        document.querySelector('button[name=signMeUp]').innerText="Sign In";
-        document.querySelector('button[name=signMeUp]').setAttribute("name","signMeIn");
-    
-        document.querySelector('a[onclick="alrMember()"]').setAttribute("onclick","createAccount()");
-    
-        document.getElementById('LoginformContainer').style.height="400px";  
-    }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,28 +8,34 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
 <body id="loginContent">
-    <div id="LoginformContainer" class="">
-        <img src="images/logo.png" alt="Freelancer.ma" class="mt-4 logoContainer">
-        <form action="includes/script.php" class="form d-flex flex-column " method="POST">
-            
-            <div id="emailFormContainer">
-                <label for="email" class="MyLabels">EMAIL</label><br>
-                <input type="email" name="email"  class="MyInputs" oninput="validateEmail()" autocomplete="off">
-                <div id="emailError" class="text-danger"></div>
-            </div>
+    <div class="d-flex flex-column flex-lg-row align-items-center justify-content-lg-around w-100">
+        <div id="first-container">
+            <h2 id="whoAreWe">Who are we ?</h2>    
+            <p id="whoAreWeText" class="pb-5 pb-lg-0">Freelancer.ma is a pioneering freelancing platform in Morocco that connects businesses and individuals with talented freelancers for a wide range of projects and services. With a focus on quality, affordability, and convenience, Freelancer.ma is dedicated to providing a seamless and secure platform for both freelancers and clients to collaborate and achieve their goals. Whether you're looking for web design, content creation, translation, or any other freelance service, Freelancer.ma has a diverse pool of skilled professionals ready to meet your needs. Join the Freelancer.ma community today and discover the limitless potential of freelancing!</p>
+        </div>
+        <div id="LoginformContainer" class="">
+            <img src="images/logo.png" alt="Freelancer.ma" class="mt-4 logoContainer">
+            <form id="auth-form" action="{{route('login')}}" class="form d-flex flex-column " method="POST">
+                @csrf
+                <div id="emailFormContainer">
+                    <label for="email" class="MyLabels">EMAIL</label><br>
+                    <input type="email" name="email"  class="MyInputs" oninput="validateEmail()" autocomplete="off">
+                    <div id="emailError" class="text-danger"></div>
+                </div>
 
-            <div class="mb-3">
-                <label for="password" class="MyLabels">PASSWORD</label><br>
-                <input type="password" name="password" class="MyInputs">
-                <div id="comfirEmailError" class="text-danger mb-0 "></div>
-            </div>
-            <div id="fullName"></div>
-            <div class="d-flex justify-content-between align-items-center mt-3" style="">
-                <span><a href="#" onclick="createAccount()">Create an account</a></span>
-                <button type="submit"  name="signMeIn" disabled>Sign In</button>
-            </div>
-        </form>
+                <div class="mb-3">
+                    <label for="password" class="MyLabels">PASSWORD</label><br>
+                    <input type="password" name="password" class="MyInputs">
+                    <div id="comfirEmailError" class="text-danger mb-0 "></div>
+                </div>
+                <div id="fullName"></div>
+                <div class="d-flex justify-content-between align-items-center mt-3" style="">
+                    <span><a href="#" onclick="createAccount()">Create an account</a></span>
+                    <button type="submit"  name="signMeIn" disabled>Sign In</button>
+                </div>
+            </form>
+        </div>
+
     </div>
-    
 </body>
 </html>
