5b3edff6ce3c3fe6c5ddb66e0affa1c1e793e605
amineelaabdi14 <amineelaabdi@gmail.com>
2023-04-18 04:53:58 +0000

    category seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public $categories = [
+        'Cleaning',
+        'Landscaping',
+        'Home Improvement',
+        'Personal Care',
+        'Baby sitting',
+        'Transportation',
+        'Education',
+        'Technology'
+    ];
+
+    public function run(): void
+    {
+        foreach($this->categories as $category){
+            Category::create(['name'=>$category]);
+        }
+    }
+}
