6325d2815b70b69e7f95d127cd16f3b2f7a974c3
amineelaabdi14 <amineelaabdi@gmail.com>
2023-04-25 23:29:58 +0100

    logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -69,8 +69,7 @@ class AuthController extends Controller
     public function logout()
     {   
         Auth::logout();
-
-        return redirect('authenticate');
+        return redirect('/login');
     }
 }
 
